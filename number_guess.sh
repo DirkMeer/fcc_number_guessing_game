384d72bdac70f8b193fed325a67b8fb14c63a409
DirkMeer <d.v.meerveld@gmail.com>
2022-08-28 17:24:16 +0000

    feat: added random number generator

--- number_guess.sh
+++ number_guess.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+RAND_NUM=$(($RANDOM % 1000 + 1)) 
+
 
 NUMBER_GUESSING() {
   echo -e "Enter your username:"
+  echo -e "\nThe random number is $RAND_NUM"
 }
 
 NUMBER_GUESSING
